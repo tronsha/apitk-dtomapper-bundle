f9f628adf06cf857994cc1c2bc8824a14b002853
Alexander Dormann <alexander.dormann@check24.de>
2018-08-16 11:19:16 +0200

    reuse common bundle; stop autowiring 3rd party services

--- Describer/AnnotationDescriber.php
+++ Describer/AnnotationDescriber.php
@@ -5,13 +5,11 @@ namespace Shopping\ApiTKDtoMapperBundle\Describer;
 use Doctrine\Common\Annotations\Annotation;
 use Doctrine\Common\Annotations\Reader;
 use EXSyst\Component\Swagger\Operation;
+use EXSyst\Component\Swagger\Path;
 use EXSyst\Component\Swagger\Response;
-use EXSyst\Component\Swagger\Swagger;
-use Nelmio\ApiDocBundle\Describer\DescriberInterface;
-use Nelmio\ApiDocBundle\Describer\ModelRegistryAwareInterface;
 use Nelmio\ApiDocBundle\Describer\ModelRegistryAwareTrait;
 use Nelmio\ApiDocBundle\Model\Model;
-use Nelmio\ApiDocBundle\Util\ControllerReflector;
+use Shopping\ApiTKCommonBundle\Describer\AbstractDescriber;
 use Shopping\ApiTKDtoMapperBundle\Service\StringHelper;
 use Symfony\Component\PropertyInfo\Type;
 use Symfony\Component\Routing\RouteCollection;
@@ -30,127 +28,71 @@ use Shopping\ApiTKDtoMapperBundle\Annotation as Dto;
  *
  * @package Shopping\ApiTKDtoMapperBundle\Describer
  */
-class AnnotationDescriber implements DescriberInterface, ModelRegistryAwareInterface
+class AnnotationDescriber extends AbstractDescriber
 {
     use ModelRegistryAwareTrait;
 
     /**
-     * @var RouteCollection
-     */
-    private $routeCollection;
-    /**
-     * @var ControllerReflector
-     */
-    private $controllerReflector;
-    /**
-     * @var Reader
-     */
-    private $reader;
-    /**
-     * @var StringHelper
-     */
+      * @var StringHelper
+      */
     private $stringHelper;
 
-    //TODO: Replace ControllerReflector with something we can depend on
     /**
-     * @param RouteCollection $routeCollection
-     * @param ControllerReflector $controllerReflector
-     * @param Reader $reader
-     * @param StringHelper $stringHelper
+     * AnnotationDescriber constructor.
+     *
+     * @param RouteCollection                                      $routeCollection
+     * @param \Shopping\ApiTKCommonBundle\Util\ControllerReflector $controllerReflector
+     * @param Reader                                               $reader
+     * @param StringHelper                                         $stringHelper
      */
     public function __construct(
         RouteCollection $routeCollection,
-        ControllerReflector $controllerReflector,
+        \Shopping\ApiTKCommonBundle\Util\ControllerReflector $controllerReflector,
         Reader $reader,
         StringHelper $stringHelper
     ) {
-        $this->routeCollection = $routeCollection;
-        $this->controllerReflector = $controllerReflector;
-        $this->reader = $reader;
+        parent::__construct($routeCollection, $controllerReflector, $reader);
         $this->stringHelper = $stringHelper;
     }
 
-    /**
-     * @param Swagger $api
-     */
-    public function describe(Swagger $api)
-    {
-        $paths = $api->getPaths();
-        foreach ($paths as $uri => $path) {
-            foreach ($path->getMethods() as $method) {
-                /** @var Operation $operation */
-                $operation = $path->getOperation($method);
-
-                foreach ($this->getMethodsToParse() as $classMethod => list($methodPath, $httpMethods)) {
-                    if ($methodPath === $uri && in_array($method, $httpMethods)) {
-                        $responseExisting = $operation->getResponses()->has(200);
-                        if ($responseExisting) {
-                            continue;
-                        }
-
-                        $view = $this->getView($this->reader->getMethodAnnotations($classMethod));
-                        if (!$view) {
-                            continue;
-                        }
-
-                        $type = $this->getDtoByMapper($view->getDtoMapper());
-                        if (!$type) {
-                            continue;
-                        }
-
-                        $shortType = $this->stringHelper->getShortTypeByType($type);
-                        if (!$shortType) {
-                            continue;
-                        }
-
-                        $isArray = $this->willReturnArray($classMethod);
-
-                        $reference = $this->modelRegistry->register(new Model(new Type(Type::BUILTIN_TYPE_OBJECT, false, $type)));
-
-                        $operation->getResponses()->set(200, $this->getResponse($isArray, $shortType, $reference));
-                    }
-                }
-            }
-        }
-    }
 
     /**
-     * @return \Generator
+     * @param Operation         $operation
+     * @param \ReflectionMethod $classMethod
+     * @param Path              $path
+     * @param string            $method
      */
-    private function getMethodsToParse(): \Generator
-    {
-        foreach ($this->routeCollection->all() as $route) {
-            if (!$route->hasDefault('_controller')) {
-                continue;
-            }
-
-            $controller = $route->getDefault('_controller');
-            if ($callable = $this->controllerReflector->getReflectionClassAndMethod($controller)) {
-                $path = $this->normalizePath($route->getPath());
-                $httpMethods = $route->getMethods() ?: Swagger::$METHODS;
-                $httpMethods = array_map('strtolower', $httpMethods);
-                $supportedHttpMethods = array_intersect($httpMethods, Swagger::$METHODS);
-
-                if (empty($supportedHttpMethods)) {
-                    continue;
-                }
-
-                yield $callable[1] => [$path, $supportedHttpMethods];
-            }
-        }
-    }
-
-    /**
-     * @param string $path
-     * @return string
-     */
-    private function normalizePath(string $path): string
-    {
-        if ('.{_format}' === substr($path, -10)) {
-            $path = substr($path, 0, -10);
+    protected function handleOperation(
+        Operation $operation,
+        \ReflectionMethod $classMethod,
+        Path $path,
+        string $method
+    ): void {
+        $responseExisting = $operation->getResponses()->has(200);
+        if ($responseExisting) {
+            return;
         }
 
-        return $path;
+        $view = $this->getView($this->reader->getMethodAnnotations($classMethod));
+        if (!$view) {
+            return;
+        }
+
+        $type = $this->getDtoByMapper($view->getDtoMapper());
+        if (!$type) {
+            return;
+        }
+
+        $shortType = $this->stringHelper->getShortTypeByType($type);
+        if (!$shortType) {
+            return;
+        }
+
+        $isArray = $this->willReturnArray($classMethod);
+
+        $reference = $this->modelRegistry->register(new Model(new Type(Type::BUILTIN_TYPE_OBJECT, false, $type)));
+
+        $operation->getResponses()->set(200, $this->getResponse($isArray, $shortType, $reference));
     }
 
     /**
