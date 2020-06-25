7cc81ca5c00936eae2447ff681a0c68fe88b3b19
Alexander Dormann <alexander.dormann@check24.de>
2020-06-25 10:32:05 +0200

    update to fos rest bundle 3.0, drop support for 2.x, symfony 4.3 and php 7.1

---
+++ Serializer/DtoEncoder.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopping\ApiTKDtoMapperBundle\Serializer;
+
+use Exception;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+use Symfony\Component\Serializer\Encoder\EncoderInterface;
+use Throwable;
+
+/**
+ * Class DtoEncoder.
+ *
+ * Registered as serializer.encoder, this DtoEncoder allows serialization of exceptions
+ *
+ * @package Shopping\ApiTKDtoMapperBundle\Serializer
+ */
+class DtoEncoder implements EncoderInterface, DecoderInterface
+{
+    public const FORMAT = 'dto';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function encode($data, /* string */$format, array $context = [])
+    {
+        $exception = $context['exception'] ?? null;
+        $isDebug = $context['debug'] ?? false;
+
+        // Use simplified exception because serialization of closures inside the real exception is not allowed and crashes
+        if ($exception instanceof Throwable) {
+            /** @var FlattenException $data */
+            $data = FlattenException::createFromThrowable($exception);
+
+            // anonymize stack trace when not in debug mode
+            if (!$isDebug) {
+                $data
+                    ->setFile('')
+                    ->setLine('')
+                    ->setTrace('', '', '')
+                    ->setPrevious(FlattenException::create(new Exception('')));
+            }
+        }
+
+        return serialize($data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function decode(/* string */$data, /* string */$format, array $context = [])
+    {
+        return unserialize($data, ['allowed_classes' => $context['allowed_classes'] ?? true]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsEncoding(/* string */$format): bool
+    {
+        return $format === self::FORMAT;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDecoding(/* string */$format): bool
+    {
+        return $format === self::FORMAT;
+    }
+}
